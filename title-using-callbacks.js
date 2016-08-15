3d0ba3a9bcae0a952bad1c883214926779531101
Saad Bin Shahid <saadbinshahid.fb@gmail.com>
2016-08-15 17:50:44 +0500

    RAW - Commit

--- title-using-callbacks.js
+++ title-using-callbacks.js
@@ -2,92 +2,98 @@ var http = require('http');
 var express = require('express');
 var app = express();
 app.set('view engine', 'pug');
+var _url = require('url');
 
 app.get('/I/want/title/', iWantTitle);
 
-app.all('*', function (req, res) {
-  res.sendStatus(404);
+app.all('*', function(req, res) {
+    res.sendStatus(404);
 });
 
-app.listen(3000, function () {
-  console.log('Server listening on port 3000!');
+app.listen(3000, function() {
+    console.log('Server listening on port 3000!');
 });
 
 
 
 // function to handle the request
 function iWantTitle(req, res) {
-  if(!req.query.address) res.send('No address passed.');
+    if (!req.query.address) res.send('No address passed.');
 
 
 
-  // counts the number of requests done
-  var done = 0;
-  // stores the requests result
-  var result = [];
+    // counts the number of requests done
+    var done = 0;
+    // stores the requests result
+    var result = [];
 
-  var re = /(<\s*title[^>]*>(.+?)<\s*\/\s*title)>/gi;
+    var re = /(<\s*title[^>]*>(.+?)<\s*\/\s*title)>/gi;
 
-  var arrAddress = [];
+    var arrAddress = [];
 
-  // get addresses
-  _address = req.query.address;
+    // get addresses
+    _address = req.query.address;
 
-  console.log(Array.isArray(_address));
-  console.log('_address', _address);
+    console.log(Array.isArray(_address));
+    console.log('_address', _address);
 
-  Array.isArray(_address) ? arrAddress = _address : arrAddress.push(_address)
-  console.log('arrAddress', arrAddress);
-  arrAddress.forEach(processUrl);
-
-
-  // process url
-  function processUrl(url, index, array) {
-    console.log('processUrl', url);
-
-    var finalData = '';
-    http.get(url, function(response) {
-      response.setEncoding('utf8');
-      response.on('data', function(data) {
-        var match = re.exec(data.toString());
-        if (match && match[2]) {
-          console.log(match[2]);
-          callback(index, match[2], array, url);
-        }
-        else {
-            finalData += data;
-        }
-      });
-
-      response.on('end', function() {
-          var match = re.exec(finalData);
-          if (match && match[2]) {
-            console.log(match[2]);
-            callback(index, match[2], array);
-          }
-      })
-    }).on('error', function(){
-          console.error('errrrrr');
-          callback(index, 'NO RESPONSE', array);
-        });;
-  }
+    Array.isArray(_address) ? arrAddress = _address : arrAddress.push(_address)
+    console.log('arrAddress', arrAddress);
+    arrAddress.forEach(processUrl);
 
 
-  // this will be called by each http.get and they will provide their index
-  function callback(index, data, array, url) {
-    console.log('callback', data);
-    console.log('index', index);
-    console.log('done', done);
-    console.log('lenght', array.length);
+    // process url
+    function processUrl(url, index, array) {
+        console.log('processUrl', _url.parse(url, true));
 
-    result[index] = {'url' : url, 'title': data};
-    done++;
-    // all requests are done, log everything
-    if (done == array.length) {
-      result.forEach(console.log);
-      res.render('index', { title: 'Hey', titles: result});
+        var finalData = '';
+        http.get(_url.format(url, true), function(response) {
+            response.setEncoding('utf8');
+            response.on('data', function(data) {
+                var match = re.exec(data.toString());
+                if (match && match[2]) {
+                    console.log(match[2]);
+                    callback(index, match[2], array, url);
+                } else {
+                    finalData += data;
+                }
+            });
+
+            response.on('end', function() {
+                var match = re.exec(finalData);
+                if (match && match[2]) {
+                    console.log(match[2]);
+                    callback(index, match[2], array);
+                }
+            })
+        }).on('error', function() {
+            console.error('errrrrr');
+            callback(index, 'NO RESPONSE', array, url);
+        });
     }
-  }
+
+
+    // this will be called by each http.get and they will provide their index
+    function callback(index, data, array, url) {
+        console.log('callback', data);
+        console.log('index', index);
+        console.log('done', done);
+        console.log('lenght', array.length);
+
+        result[index] = {
+            'url': url,
+            'title': data
+        };
+        done++;
+        // all requests are done, log everything
+        if (done == array.length) {
+            result.forEach(console.log);
+            res.render('index', {
+                title: 'Hey',
+                titles: result
+            });
+        }
+    }
 
 
 };
--- title-using-promises.js
+++ title-using-promises.js
@@ -1,52 +1,114 @@
+var http = require('http');
 var express = require('express');
-var scrapper = require('./title-scrapper');
 var app = express();
-var Q = require('q');
-
-console.log(scrapper);
+app.set('view engine', 'pug');
 
 app.get('/I/want/title/', iWantTitle);
 
-app.all('*', function (req, res) {
-  res.sendStatus(404);
+app.all('*', function(req, res) {
+    res.sendStatus(404);
 });
 
-app.listen(3000, function () {
-  console.log('Server listening on port 3000!');
+app.listen(3000, function() {
+    console.log('Server listening on port 3000!');
 });
 
 
 
-
+// function to handle the request
 function iWantTitle(req, res) {
+    if (!req.query.address) res.send('No address passed.');
 
-  // get addresses
-  arrAddress = req.query.address;
 
-  // has address ?
-  if(arrAddress != undefined && arrAddress.length > 0){
 
-    allPromises = [];
+    // counts the number of requests done
+    var done = 0;
+    // stores the requests result
+    var result = [];
 
-    arrAddress.forEach(function(url){
-      console.log(url);
-      arrNfcCalls.push(scrapper.getTitle(url));
-    });
+    var re = /(<\s*title[^>]*>(.+?)<\s*\/\s*title)>/gi;
 
-    // using q
-    Q.all(allPromises).then(
-        function (results) {
-          console.log('results:', results);
-        },
-        function (err) {
-          console.error('ERR', err);
+    var arrAddress = [];
+
+    // get addresses
+    _address = req.query.address;
+
+    console.log(Array.isArray(_address));
+    console.log('_address', _address);
+
+    Array.isArray(_address) ? arrAddress = _address : arrAddress.push(_address)
+    console.log('arrAddress', arrAddress);
+    arrAddress.forEach(processUrl);
+
+
+    // process url
+    function processUrl(url) {
+        console.log('processUrl', url);
+
+        return Q.Promise(function(resolve, reject, notify) {
+            var request = new XMLHttpRequest();
+
+            request.open("GET", url, true);
+            request.onload = onload;
+            request.onerror = onerror;
+            request.onprogress = onprogress;
+            request.send();
+
+            function onload() {
+                if (request.status === 200) {
+                    resolve(request.responseText);
+                } else {
+                    reject(new Error("Status code was " + request.status));
+                }
+            }
+
+            function onerror() {
+                reject(new Error("Can't XHR " + JSON.stringify(url)));
+            }
+
+            function onprogress(event) {
+                notify(event.loaded / event.total);
+            }
+        });
+
+
+    }
+
+
+    // this will be called by each http.get and they will provide their index
+    function callback(index, data, array, url) {
+        console.log('callback', data);
+        console.log('index', index);
+        console.log('done', done);
+        console.log('lenght', array.length);
+
+        result[index] = {
+            'url': url,
+            'title': data
+        };
+        done++;
+        // all requests are done, log everything
+        if (done == array.length) {
+            result.forEach(console.log);
+            res.render('index', {
+                title: 'Hey',
+                titles: result
+            });
         }
-    );
+    }
 
-
-  }
-  else {
-    res.send('No address passed.');
-  }
+    // promises
+    Q.allSettled(promises)
+    .then(function (results) {
+        results.forEach(function (result) {
+            if (result.state === "fulfilled") {
+                var value = result.value;
+            } else {
+                var reason = result.reason;
+            }
+        });
+    }, function (error) {
+    // We only get here if "foo" fails
+    });
 
 };
