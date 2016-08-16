7fa884f13d085aedfaec9b22912e4f53eb579c18
Saad Bin Shahid <saadbinshahid.fb@gmail.com>
2016-08-16 11:55:08 +0500

    Added promises-stub and template.

--- title-using-promises.js
+++ title-using-promises.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var express = require('express');
+var Q = require('q');
 var app = express();
 app.set('view engine', 'pug');
 
@@ -29,6 +30,8 @@ function iWantTitle(req, res) {
     var re = /(<\s*title[^>]*>(.+?)<\s*\/\s*title)>/gi;
 
     var arrAddress = [];
+    var promises = [];
+    var arrResults = [];
 
     // get addresses
     _address = req.query.address;
@@ -38,77 +41,108 @@ function iWantTitle(req, res) {
 
     Array.isArray(_address) ? arrAddress = _address : arrAddress.push(_address)
     console.log('arrAddress', arrAddress);
-    arrAddress.forEach(processUrl);
+    arrAddress.forEach(function(url){
+      promises.push(processUrl(url));
+    });
 
+    // promises
+    Q.allSettled(promises)
+    .then(function (results) {
+        results.forEach(function (result) {
+          console.log('result >> ', result);
+          arrResults.push(
+              {
+                  'url': result.value.url,
+                  'title': result.value.title
+              });
+        });
+    })
+    .done(function(){
+            console.log('arrResults', arrResults);
+            res.render('index', {
+                title: 'Hey',
+                titles: arrResults
+            });
+  });
 
     // process url
     function processUrl(url) {
         console.log('processUrl', url);
 
         return Q.Promise(function(resolve, reject, notify) {
-            var request = new XMLHttpRequest();
+            // var request = new XMLHttpRequest();
+            //
+            // request.open("GET", url, true);
+            // request.onload = onload;
+            // request.onerror = onerror;
+            // request.onprogress = onprogress;
+            // request.send();
 
-            request.open("GET", url, true);
-            request.onload = onload;
-            request.onerror = onerror;
-            request.onprogress = onprogress;
-            request.send();
 
-            function onload() {
-                if (request.status === 200) {
-                    resolve(request.responseText);
-                } else {
-                    reject(new Error("Status code was " + request.status));
-                }
-            }
+            var finalData = '';
+            http.get(url, function(response) {
+                response.setEncoding('utf8');
+                response.on('data', function(data) {
+                    var match = re.exec(data.toString());
+                    if (match && match[2]) {
+                        console.log(match[2]);
+                        var data = {
+                          title: match[2],
+                          url: url
+                        };
+                        resolve(data);
+                    } else {
+                        finalData += data;
+                    }
+                });
 
-            function onerror() {
-                reject(new Error("Can't XHR " + JSON.stringify(url)));
-            }
+                response.on('end', function() {
+                    var match = re.exec(finalData);
+                    if (match && match[2]) {
+                        console.log(match[2]);
+                        var data = {
+                          title: match[2],
+                          url: url
+                        };
+                        resolve(data);
+                    }
+                })
+            }).on('error', function() {
+                console.error('errrrrr');
+                var data = {
+                  title: 'NO RESPONSE',
+                  url: url
+                };
+                resolve(data);
+            });
 
-            function onprogress(event) {
-                notify(event.loaded / event.total);
-            }
         });
-
 
     }
 
 
     // this will be called by each http.get and they will provide their index
-    function callback(index, data, array, url) {
-        console.log('callback', data);
-        console.log('index', index);
-        console.log('done', done);
-        console.log('lenght', array.length);
+    // function callback(index, data, array, url) {
+    //     console.log('callback', data);
+    //     console.log('index', index);
+    //     console.log('done', done);
+    //     console.log('lenght', array.length);
+    //
+    //     result[index] = {
+    //         'url': url,
+    //         'title': data
+    //     };
+    //     done++;
+    //     // all requests are done, log everything
+    //     if (done == array.length) {
+    //         result.forEach(console.log);
+    //         res.render('index', {
+    //             title: 'Hey',
+    //             titles: result
+    //         });
+    //     }
+    // }
 
-        result[index] = {
-            'url': url,
-            'title': data
-        };
-        done++;
-        // all requests are done, log everything
-        if (done == array.length) {
-            result.forEach(console.log);
-            res.render('index', {
-                title: 'Hey',
-                titles: result
-            });
-        }
-    }
 
-    // promises
-    Q.allSettled(promises)
-    .then(function (results) {
-        results.forEach(function (result) {
-            if (result.state === "fulfilled") {
-                var value = result.value;
-            } else {
-                var reason = result.reason;
-            }
-        });
-    }, function (error) {
-    // We only get here if "foo" fails
-    });
 
 };
